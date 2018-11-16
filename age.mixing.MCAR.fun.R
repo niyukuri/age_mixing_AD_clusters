62a514957333c1d3333a2c309a335dc23617aaa2
Mr D Niyukuri <niyukuri@aims.ac.za>
2018-11-16 09:12:58 +0200

    adding age mix pattern stat in description of output for MCAR

--- age.mixing.MCAR.fun.R
+++ age.mixing.MCAR.fun.R
@@ -60,6 +60,10 @@
 # table.tree.trans.true.age.str.WtoM.prop.men, table.tree.trans.true.age.str.prop.women,
 # numbers.individuals.age.groups.net, mean.AD.age.groups.net, med.AD.age.groups.net, sd.AD.age.groups.net
 
+# (iv) True age mixing patterns in relationships
+
+# "T.AAD.male", "T.SDAD.male", "T.slope.male", "T.WSD.male", "T.BSD.male", "T.intercept.male"
+# "T.p.prev.6months.m", # "T.p.prev.6months.f",
 
 # (iv) Transmission clusters
 
